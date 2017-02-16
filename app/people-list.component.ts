c678998e2df1a51bc920dea89631da1d42c6267c
JulianSauer <julian_sauer@gmx.net>
2017-02-16 19:40:32 +0100

    Added details on the characters and improved visuals.

--- app/people-list.component.ts
+++ app/people-list.component.ts
@@ -5,17 +5,23 @@ import {PeopleService} from "./people.service";
 @Component({
   selector: 'people-list',
   templateUrl: 'app/people-list.component.html',
+  styleUrls: ['app/people-list.component.css']
 })
 
 export class PeopleListComponent implements OnInit {
 
   people: Person[] = [];
+  selectedPerson: Person;
 
   constructor(private peopleService: PeopleService) {
   }
 
   ngOnInit() {
     this.people = this.peopleService.getAll();
+  }
+
+  selectPerson(person: Person) {
+    this.selectedPerson = person;
   }
 
 }
